b23962a458fe2ea2485ed22f340e5229db7b259d
shafkat63 <msm.shafkat99@gmail.com>
2024-07-18 12:18:48 +0600

    Option and composition API

--- src/App.vue
+++ src/App.vue
@@ -1,85 +1,57 @@
-<script setup>
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
+<script>
+import { ref } from "vue";
+
+export default {
+	setup() {
+		const name = ref("Ezio Altair");
+		const status = ref("active");
+		const tasks = ref([
+			"Task one",
+			"Task two",
+			"Task three",
+			"Task four",
+			"Task five",
+			"Task six",
+			"Task seven",
+			"Task eight",
+			"Task nine",
+			"Task ten",
+		]);
+
+		const toggleStatus = () => {
+			if (status.value === "active") {
+				status.value = "pending";
+			} else if (status.value === "pending") {
+				status.value = "inactive";
+			} else {
+				status.value = "active";
+			}
+		};
+
+		return {
+			name,
+			status,
+			tasks,
+			toggleStatus,
+		};
+	},
+};
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
-  </header>
-
-  <RouterView />
+	<h1>{{ name }}</h1>
+	<p v-if="status === 'active'">User is Active</p>
+	<p v-else-if="status === 'pending'">User is Pending</p>
+	<p v-else>User is InActive</p>
+	<h3>Tasks:</h3>
+	<ul>
+		<li v-for="task in tasks" :key="task">
+			{{ task }}
+		</li>
+	</ul>
+	<!-- <a v-bind:href="link">Click for MDN</a> -->
+	<a :href="link">Click for MDN</a>
+	<br />
+	<!-- <button v-on:click="toggleStatus" >Change Status</button> -->
+	<button @click="toggleStatus">Change Status</button>
 </template>
-
-<style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
-}
-</style>
---
+++ src/App2.vue
@@ -0,0 +1,52 @@
+<script>
+export default {
+	data() {
+		return {
+			name: "Ezio Altair",
+			status: "pending",
+			tasks: [
+				"Task one",
+				"Task two",
+				"Task three",
+				"Task four",
+				"Task five",
+				"Task six",
+				"Task seven",
+				"Task eight",
+				"Task nine",
+				"Task ten",
+			],
+			link: "https://developer.mozilla.org",
+		};
+	},
+	methods: {
+		toggleStatus() {
+			if (this.status === "active") {
+				this.status = "pending";
+			} else if (this.status === "pending") {
+				this.status = "inactive";
+			} else {
+				this.status = "active";
+			}
+		},
+	},
+};
+</script>
+<template>
+	<h1>{{ name }}</h1>
+	<p v-if="status === 'active'">User is Active</p>
+	<p v-else-if="status === 'pending'">User is Pending</p>
+	<p v-else>User is InActive</p>
+	<h3>Tasks:</h3>
+	<ul>
+		<li v-for="task in tasks" :key="task">
+			{{ task }}
+		</li>
+	</ul>
+	<!-- <a v-bind:href="link">Click for MDN</a> -->
+	<a :href="link">Click for MDN</a>
+	<br />
+	<!-- <button v-on:click="toggleStatus" >Change Status</button> -->
+	<button @click="toggleStatus" >Change Status</button>
+</template>
+
